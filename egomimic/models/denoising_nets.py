15002a829ff6e12f4318a2370774dbc8525ac733
ElmoPA <elmoworld2005@gmail.com>
2026-09-02 21:25:59 -0400

    refactor(models): prune obsolete denoisers

--- egomimic/models/denoising_nets.py
+++ egomimic/models/denoising_nets.py
@@ -83,12 +83,6 @@ class Conv1dBlock(nn.Module):
         return self.block(x)
 
 
-def test():
-    cb = Conv1dBlock(256, 128, kernel_size=3)
-    x = torch.zeros((1, 256, 16))
-    cb(x)
-
-
 class SinusoidalPosEmb(nn.Module):
     def __init__(self, dim):
         super().__init__()
@@ -177,9 +171,7 @@ class ConditionalUnet1D(nn.Module):
         n_groups=8,
         cond_predict_scale=False,
     ):
-        """
-        local conditioning and global conditioning scheme
-        """
+        """Build a one-dimensional U-Net with global conditioning."""
         super().__init__()
         all_dims = [input_dim] + list(down_dims)
         start_dim = down_dims[0]
@@ -281,7 +273,6 @@ class ConditionalUnet1D(nn.Module):
         )
 
         self.diffusion_step_encoder = diffusion_step_encoder
-        self.local_cond_encoder = local_cond_encoder
         self.up_modules = up_modules
         self.down_modules = down_modules
         self.final_conv = final_conv
@@ -301,7 +292,6 @@ class ConditionalUnet1D(nn.Module):
         """
         sample: (B,T,input_dim)
         timestep: (B,) or int, diffusion step
-        local_cond: (B,T,local_cond_dim)
         global_cond: (B,global_cond_dim)
         output: (B,T,input_dim)
         """
@@ -355,12 +345,11 @@ class ConditionalUnet1D(nn.Module):
 
 
 class PaperConditionalUnet1D(nn.Module):
-    """Checkpoint-compatible U-Net from the published Diffusion Policy image model."""
+    """Diffusion Policy U-Net with global observation conditioning."""
 
     def __init__(
         self,
         input_dim,
-        local_cond_dim=None,
         global_cond_dim=None,
         diffusion_step_embed_dim=256,
         down_dims=(256, 512, 1024),
@@ -393,16 +382,6 @@ class PaperConditionalUnet1D(nn.Module):
                 cond_predict_scale=bool(cond_predict_scale),
             )
 
-        self.local_cond_encoder = None
-        if local_cond_dim is not None:
-            first_output = pairs[0][1]
-            self.local_cond_encoder = nn.ModuleList(
-                [
-                    residual(int(local_cond_dim), first_output),
-                    residual(int(local_cond_dim), first_output),
-                ]
-            )
-
         middle = all_dims[-1]
         self.mid_modules = nn.ModuleList(
             [residual(middle, middle), residual(middle, middle)]
@@ -431,7 +410,7 @@ class PaperConditionalUnet1D(nn.Module):
                         Upsample1d(input_channels),
                     ]
                 )
-                for index, (input_channels, output_channels) in enumerate(reverse_pairs)
+                for input_channels, output_channels in reverse_pairs
             ]
         )
         self.final_conv = nn.Sequential(
@@ -448,8 +427,6 @@ class PaperConditionalUnet1D(nn.Module):
         sample: torch.Tensor,
         timestep: Union[torch.Tensor, float, int],
         global_cond=None,
-        local_cond=None,
-        **_kwargs,
     ):
         sample = einops.rearrange(sample, "b h t -> b t h")
         timesteps = timestep
@@ -470,193 +447,19 @@ class PaperConditionalUnet1D(nn.Module):
                 )
             condition = torch.cat((condition, global_cond), dim=-1)
 
-        local_features = []
-        if local_cond is not None:
-            if self.local_cond_encoder is None:
-                raise ValueError("local_cond was supplied but local_cond_dim was not")
-            local_cond = einops.rearrange(local_cond, "b h t -> b t h")
-            local_features = [
-                block(local_cond, condition) for block in self.local_cond_encoder
-            ]
-
         x = sample
         residuals = []
-        for index, (first, second, downsample) in enumerate(self.down_modules):
+        for first, second, downsample in self.down_modules:
             x = first(x, condition)
-            if index == 0 and local_features:
-                x = x + local_features[0]
             x = second(x, condition)
             residuals.append(x)
             x = downsample(x)
         for block in self.mid_modules:
             x = block(x, condition)
-        for index, (first, second, upsample) in enumerate(self.up_modules):
+        for first, second, upsample in self.up_modules:
             x = first(torch.cat((x, residuals.pop()), dim=1), condition)
-            # This unreachable condition is deliberate: the reference model's
-            # checkpoints contain this local-condition compatibility quirk.
-            if index == len(self.up_modules) and local_features:
-                x = x + local_features[1]
             x = upsample(second(x, condition))
         return einops.rearrange(self.final_conv(x), "b t h -> b h t")
-
-
-class ConditionalClassifier1D(nn.Module):
-    def __init__(
-        self,
-        input_dim,
-        local_cond_dim=None,
-        global_cond_dim=None,
-        diffusion_step_embed_dim=256,
-        down_dims=[256, 512, 1024],
-        kernel_size=3,
-        n_groups=8,
-        cond_predict_scale=False,
-    ):
-        """
-        local conditioning and global conditioning scheme
-        """
-        super().__init__()
-        start_dim = down_dims[0]
-
-        dsed = diffusion_step_embed_dim
-        diffusion_step_encoder = nn.Sequential(
-            SinusoidalPosEmb(dsed),
-            nn.Linear(dsed, dsed * 4),
-            nn.Mish(),
-            nn.Linear(dsed * 4, dsed),
-        )
-        cond_dim = dsed
-        if global_cond_dim is not None:
-            cond_dim += global_cond_dim
-
-        self.resnet = ConditionalResidualBlock1D(
-            input_dim,
-            start_dim,
-            cond_dim=cond_dim,
-            kernel_size=kernel_size,
-            n_groups=n_groups,
-            cond_predict_scale=cond_predict_scale,
-        )
-
-        final_conv = nn.Sequential(
-            Conv1dBlock(start_dim, start_dim, kernel_size=kernel_size),
-            nn.Conv1d(start_dim, 1, 1),
-        )
-
-        self.diffusion_step_encoder = diffusion_step_encoder
-        self.final_conv = final_conv
-
-        logger.info(
-            "number of parameters: %e", sum(p.numel() for p in self.parameters())
-        )
-
-    def forward(
-        self,
-        sample: torch.Tensor,
-        timestep: Union[torch.Tensor, float, int],
-        local_cond=None,
-        global_cond=None,
-        **kwargs,
-    ):
-        """
-        sample: (B,T,input_dim)
-        timestep: (B,) or int, diffusion step
-        local_cond: (B,T,local_cond_dim)
-        global_cond: (B,global_cond_dim)
-        output: (B,T,input_dim)
-        """
-
-        sample = einops.rearrange(sample, "b h t -> b t h")
-
-        # 1. time
-        timesteps = timestep
-        if not torch.is_tensor(timesteps):
-            # TODO: this requires sync between CPU and GPU. So try to pass timesteps as tensors if you can
-            timesteps = torch.tensor(
-                [timesteps], dtype=torch.long, device=sample.device
-            )
-        elif torch.is_tensor(timesteps) and len(timesteps.shape) == 0:
-            timesteps = timesteps[None].to(sample.device)
-
-        # broadcast to batch dimension in a way that's compatible with ONNX/Core ML
-        timesteps = timesteps.expand(sample.shape[0])
-        global_feature = self.diffusion_step_encoder(timesteps)
-
-        if global_cond is not None:
-            global_feature = torch.cat([global_feature, global_cond], axis=-1)
-
-        x = self.resnet(sample, global_feature)
-        x = self.final_conv(x)
-        x = einops.rearrange(x, "b t h -> b h t")
-        return x
-
-
-class AdaptiveLayerNorm(nn.Module):
-    def __init__(self, cond_dim, hidden_dim, n_layers, shift=True):
-        super().__init__()
-        self.hidden_dim = hidden_dim
-        self.n_layers = n_layers
-        self.cond_dim = cond_dim
-        layers = [nn.Linear(cond_dim, hidden_dim), nn.SiLU()]
-        for i in range(n_layers - 1):
-            layers += [nn.Linear(hidden_dim, hidden_dim), nn.SiLU()]
-        output_dim = hidden_dim * 2 if shift else hidden_dim
-        self.shift = shift
-        layers.append(nn.Linear(hidden_dim, output_dim))
-        self.mlp = nn.Sequential(*layers)
-
-    def forward(self, x, t, cond, time_table):
-        cond = cond.unsqueeze(1) + time_table[t]
-        out = self.mlp(cond)
-        if self.shift:
-            return x * (1 + out[..., : self.hidden_dim]) + out[..., self.hidden_dim :]
-        else:
-            return x * (1 + out)
-
-
-class DiTBlock(nn.Module):
-    def __init__(
-        self,
-        cond_dim,
-        hidden_dim,
-        n_heads,
-        dropout,
-        mlp_layers,
-        mlp_ratio,
-        ad_nlayers,
-    ):
-        super().__init__()
-        self.hidden_dim = hidden_dim
-        self.ln1 = nn.LayerNorm(hidden_dim)
-        self.aln1 = AdaptiveLayerNorm(cond_dim, hidden_dim, ad_nlayers)
-        self.mha = nn.MultiheadAttention(
-            embed_dim=hidden_dim, num_heads=n_heads, dropout=dropout, batch_first=True
-        )
-        self.alns1 = AdaptiveLayerNorm(cond_dim, hidden_dim, ad_nlayers, False)
-        self.ln2 = nn.LayerNorm(hidden_dim)
-        self.aln2 = AdaptiveLayerNorm(cond_dim, hidden_dim, ad_nlayers)
-        mlp_dim_size = int(hidden_dim * mlp_ratio)
-        layers = [nn.Linear(hidden_dim, mlp_dim_size), nn.GELU()]
-        for i in range(mlp_layers - 1):
-            layers.append(nn.Linear(mlp_dim_size, mlp_dim_size))
-            layers.append(nn.GELU())
-        layers.append(nn.Linear(mlp_dim_size, hidden_dim))
-        self.mlp = nn.Sequential(*layers)
-        self.alns2 = AdaptiveLayerNorm(cond_dim, hidden_dim, ad_nlayers)
-
-    def forward(self, x, t, cond, timestep_table):
-        res = x
-        x = self.ln1(x)
-        x = self.aln1(x, t, cond, timestep_table)
-        (x, _) = self.mha(x, x, x)
-        x = self.alns1(x, t, cond, timestep_table)
-        x = x + res
-        res = x
-        x = self.ln2(x)
-        x = self.aln2(x, t, cond, timestep_table)
-        x = self.mlp(x)
-        x = self.alns2(x, t, cond, timestep_table) + res
-        return x
 
 
 class CrossBlock(nn.Module):
@@ -707,48 +510,6 @@ class CrossBlock(nn.Module):
     def forward(self, x, cond):
         cond = self.cond_proj(cond)
         return self.forward_cross(x, cond)
-
-
-class CrossBlockCfg2(CrossBlock):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.time_proj = nn.Linear(self.hidden_dim, self.hidden_dim)
-
-    def forward(self, x, cond, time):
-        time = self.time_proj(time)
-        cond = self.cond_proj(cond)
-        cond_time = torch.cat([cond, time], dim=-2)
-        return super().forward_cross(x, cond_time)
-
-
-# class AdaLnTransformer(nn.Module):
-#     def __init__(
-#         self,
-#         nblocks,
-#         cond_dim,
-#         hidden_dim,
-#         act_dim,
-#         n_heads,
-#         dropout,
-#         mlp_layers,
-#         mlp_ratio,
-#         ad_nlayers,
-#         num_train_timesteps
-#     ):
-#         super().__init__()
-#         timestep_table = torch.stack([timestep_embedding(torch.tensor([i], dtype=torch.int), cond_dim)
-#                      for i in range(num_train_timesteps)], dim=0)
-#         self.register_buffer('timestep_table', timestep_table)
-#         self.layers = nn.ModuleList([DiTBlock(cond_dim, hidden_dim, n_heads, dropout, mlp_layers, mlp_ratio, ad_nlayers) for i in range(nblocks)])
-#         self.proj_u = nn.Linear(act_dim, hidden_dim)
-#         self.proj_d = nn.Linear(hidden_dim, act_dim)
-
-#     def forward(self, x, timesteps, global_cond):
-#         hid_tkns = self.proj_u(x)
-#         for layer in self.layers:
-#             hid_tkns = layer(hid_tkns, timesteps, global_cond, self.timestep_table)
-#         x = self.proj_d(hid_tkns)
-#         return x
 
 
 class CrossTransformer(nn.Module):
@@ -810,8 +571,6 @@ class CrossTransformer(nn.Module):
             .expand(hid_tkns.shape[0], hid_tkns.shape[1], -1)
             .to(hid_tkns.device)
         )
-        # if hasattr(timesteps, "shape") and len(timesteps.shape) > 0 and timesteps.shape[0] == 1:
-        #     breakpoint()
         if self.time_conditioning == "concat":
             hid_tkns = torch.cat([hid_tkns, time_embed], dim=-1)
         else:
@@ -820,151 +579,4 @@ class CrossTransformer(nn.Module):
         for layer in self.layers:
             hid_tkns = layer(hid_tkns, cond)
         x = self.proj_d(hid_tkns)
-        return x
-
-
-class CrossTransformerCfg2(nn.Module):
-    def __init__(
-        self,
-        nblocks,
-        cond_dim,
-        hidden_dim,
-        act_dim,
-        act_seq,
-        n_heads,
-        dropout,
-        mlp_layers,
-        mlp_ratio,
-        **kwargs,
-    ):
-        super().__init__()
-        self.layers = nn.ModuleList(
-            [
-                CrossBlockCfg2(
-                    cond_dim,
-                    hidden_dim,
-                    n_heads,
-                    dropout,
-                    mlp_layers,
-                    mlp_ratio,
-                    **kwargs,
-                )
-                for i in range(nblocks)
-            ]
-        )
-        self.proj_u = nn.Linear(act_dim, hidden_dim)
-        self.proj_d = nn.Linear(hidden_dim, act_dim)
-        self.pos_emb = nn.Parameter(torch.zeros(1, act_seq, hidden_dim))
-
-    def forward(self, x, timesteps, cond, *args, **kwargs):
-        hid_tkns = self.proj_u(x)
-        hid_tkns = hid_tkns + self.pos_emb
-        time_embed = (
-            posemb_sincos(
-                timesteps, self.proj_u.out_features, min_period=4e-3, max_period=4.0
-            )
-            .unsqueeze(1)
-            .expand(hid_tkns.shape[0], hid_tkns.shape[1], -1)
-            .to(hid_tkns.device)
-        )  # (1, S, D)
-        # if hasattr(timesteps, "shape") and len(timesteps.shape) > 0 and timesteps.shape[0] == 1:
-        #     breakpoint()
-        for layer in self.layers:
-            hid_tkns = layer(hid_tkns, cond, time_embed)
-        x = self.proj_d(hid_tkns)
-        return x
-
-
-class CrossTransformerProj(nn.Module):
-    def __init__(
-        self,
-        nblocks,
-        cond_dim,
-        hidden_dim,
-        act_dim_robot,
-        act_dim_human,
-        act_seq,
-        n_mlp_proj,
-        n_heads,
-        dropout,
-        mlp_layers,
-        mlp_ratio,
-        **kwargs,
-    ):
-        super().__init__()
-        self.layers = nn.ModuleList(
-            [
-                CrossBlock(
-                    cond_dim,
-                    hidden_dim,
-                    n_heads,
-                    dropout,
-                    mlp_layers,
-                    mlp_ratio,
-                    **kwargs,
-                )
-                for i in range(nblocks)
-            ]
-        )
-        self.proj_u_robot = nn.Linear(act_dim_robot, hidden_dim // 2)
-        self.proj_d_robot = nn.Linear(hidden_dim, act_dim_robot)
-        self.proj_u_human = nn.Linear(act_dim_human, hidden_dim // 2)
-        self.proj_d_human = nn.Linear(hidden_dim, act_dim_human)
-        self.n_mlp_proj = n_mlp_proj
-        self.hidden_dim = hidden_dim
-        if self.n_mlp_proj > 0:
-            h_mlp_u = [nn.GELU(), nn.Linear(hidden_dim // 2, hidden_dim // 2)]
-            r_mlp_u = [nn.GELU(), nn.Linear(hidden_dim // 2, hidden_dim // 2)]
-            h_mlp_d = [nn.GELU(), nn.Linear(hidden_dim, hidden_dim)]
-            r_mlp_d = [nn.GELU(), nn.Linear(hidden_dim, hidden_dim)]
-            for i in range(n_mlp_proj - 1):
-                h_mlp_u.append(nn.GELU())
-                h_mlp_u.append(nn.Linear(hidden_dim // 2, hidden_dim // 2))
-                r_mlp_u.append(nn.GELU())
-                r_mlp_u.append(nn.Linear(hidden_dim // 2, hidden_dim // 2))
-
-                h_mlp_d.append(nn.GELU())
-                h_mlp_d.append(nn.Linear(hidden_dim, hidden_dim))
-                r_mlp_d.append(nn.GELU())
-                r_mlp_d.append(nn.Linear(hidden_dim, hidden_dim))
-
-            self.h_mlp_u = nn.Sequential(*h_mlp_u)
-            self.r_mlp_u = nn.Sequential(*r_mlp_u)
-            self.h_mlp_d = nn.Sequential(*h_mlp_d)
-            self.r_mlp_d = nn.Sequential(*r_mlp_d)
-
-        self.pos_emb = nn.Parameter(torch.zeros(1, act_seq, hidden_dim // 2))
-
-    def forward(self, x, timesteps, cond, robot, *args, **kwargs):
-        if robot:
-            hid_tkns = self.proj_u_robot(x)
-            if self.n_mlp_proj > 0:
-                hid_tkns = self.r_mlp_u(hid_tkns)
-        else:
-            hid_tkns = self.proj_u_human(x)
-            if self.n_mlp_proj > 0:
-                hid_tkns = self.h_mlp_u(hid_tkns)
-        hid_tkns = hid_tkns + self.pos_emb
-        time_embed = (
-            posemb_sincos(
-                timesteps, self.hidden_dim // 2, min_period=4e-3, max_period=4.0
-            )
-            .unsqueeze(1)
-            .expand(hid_tkns.shape[0], hid_tkns.shape[1], -1)
-            .to(hid_tkns.device)
-        )  # (1, S, D)
-        hid_tkns = torch.cat([hid_tkns, time_embed], dim=-1)
-
-        # if hasattr(timesteps, "shape") and len(timesteps.shape) > 0 and timesteps.shape[0] == 1:
-        #     breakpoint()
-        for layer in self.layers:
-            hid_tkns = layer(hid_tkns, cond)
-        if robot:
-            if self.n_mlp_proj > 0:
-                hid_tkns = self.r_mlp_d(hid_tkns)
-            x = self.proj_d_robot(hid_tkns)
-        else:
-            if self.n_mlp_proj > 0:
-                hid_tkns = self.h_mlp_d(hid_tkns)
-            x = self.proj_d_human(hid_tkns)
         return x

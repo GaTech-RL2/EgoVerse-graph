d4795f929b8ac28f41379b84f9ee62cb7cb76a82
Aniketh Cheluva <acheluva3@login-ice-gnr-2.pace.gatech.edu>
2026-09-10 14:06:03 -0400

    feat(arc): replace Micro-AGI language experiments
    (cherry picked from commit 80a84f21881a00df43904336e189b64d942b4bf0)

--- egomimic/models/stems/__init__.py
+++ egomimic/models/stems/__init__.py
@@ -1,5 +1,6 @@
 """Reusable observation encoders."""
 
+from egomimic.models.stems import hpt_stems
 from egomimic.models.stems.visual_core import SpatialSoftmax, VisualCore
 
-__all__ = ["SpatialSoftmax", "VisualCore"]
+__all__ = ["SpatialSoftmax", "VisualCore", "hpt_stems"]
